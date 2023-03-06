3f6094781994906eea7feffb6b3ed2f1207b88ba
Artur Polguy <art.polguy@gmail.com>
2023-03-06 23:19:22 +0200

    change forms to hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,53 +1,92 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import css from './Searchbar.module.css';
 import PropTypes from 'prop-types';
 
-class Searchbar extends Component {
-  state = {
-    search: '',
-  };
+const Searchbar = ({ onSubmit }) => {
+  const [state, setState] = useState('');
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const { onSubmit } = this.props;
-    onSubmit({ ...this.state });
-    this.reset();
+
+    onSubmit({ ...state });
+    setState({ search: '' });
   };
 
-  reset() {
-    this.setState({ search: '' });
-  }
-
-  handleChange = e => {
-    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
+  const handleChange = ({ currentTarget }) => {
+    const { name, value } = currentTarget;
+    setState({ [name]: value });
   };
+  const { search } = state;
 
-  render() {
-    const { search } = this.state;
-    return (
-      <header className={css.searchbar}>
-        <form className={css.searchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={css.SearchFormBtn}>
-            <AiOutlineSearch className={css.btnLabel} />
-          </button>
+  return (
+    <header className={css.searchbar}>
+      <form className={css.searchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={css.SearchFormBtn}>
+          <AiOutlineSearch className={css.btnLabel} />
+        </button>
 
-          <input
-            className={css.SearchFormInput}
-            name="search"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={this.handleChange}
-            value={search}
-            required
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={css.SearchFormInput}
+          name="search"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={handleChange}
+          value={search}
+          required
+        />
+      </form>
+    </header>
+  );
+};
+
+// class Searchbar extends Component {
+//   state = {
+//     search: '',
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const { onSubmit } = this.props;
+//     onSubmit({ ...this.state });
+//     this.reset();
+//   };
+
+//   reset() {
+//     this.setState({ search: '' });
+//   }
+
+//   handleChange = e => {
+//     this.setState({ [e.currentTarget.name]: e.currentTarget.value });
+//   };
+
+//   render() {
+//     const { search } = this.state;
+//     return (
+//       <header className={css.searchbar}>
+//         <form className={css.searchForm} onSubmit={this.handleSubmit}>
+//           <button type="submit" className={css.SearchFormBtn}>
+//             <AiOutlineSearch className={css.btnLabel} />
+//           </button>
+
+//           <input
+//             className={css.SearchFormInput}
+//             name="search"
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             onChange={this.handleChange}
+//             value={search}
+//             required
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
 
 export default Searchbar;
 
